8c2a8b0890e6e6a90319fb07053e23a41be6aaf9
Muzammil Hussain <muzammil0301@gmail.com>
2023-02-08 01:48:43 +0500

    VIdeo Player

--- lib/view/live_tv_page.dart
+++ lib/view/live_tv_page.dart
@@ -1,5 +1,7 @@
 import 'package:apivideo_live_stream/apivideo_live_stream.dart';
+import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class LiveTvPage extends StatefulWidget {
   const LiveTvPage({Key? key}) : super(key: key);
@@ -9,27 +11,50 @@ class LiveTvPage extends StatefulWidget {
 }
 
 class _LiveTvPageState extends State<LiveTvPage> {
-  final ApiVideoLiveStreamController _controller = ApiVideoLiveStreamController(initialAudioConfig: AudioConfig(), initialVideoConfig: VideoConfig.withDefaultBitrate());
+  // final ApiVideoLiveStreamController _controller = ApiVideoLiveStreamController(initialAudioConfig: AudioConfig(), initialVideoConfig: VideoConfig.withDefaultBitrate());
 
+  FlickManager? flickManager;
+  VideoPlayerController? videoPlayerController;
+
+  final vidURL = 'https://expense.lemonappsg.com/public/uploads/Test Video.mp4';
   @override
   void initState() {
-    // TODO: implement initState
-    init();
+    videoPlayerController = VideoPlayerController.network(vidURL);
+    flickManager = FlickManager(videoPlayerController: videoPlayerController!, autoInitialize: true);
     super.initState();
   }
 
-  init() async {
-    await _controller.startStreaming(
-      url: 'https://embed.api.video/vod/vi2NcENcGeKc4c1rIDgsv4Yx',
-      streamKey: '8BGYGRfPuqs6zaFHafdzNPgP6Uim44mkyKHHYkr1CfH',
-    );
+  @override
+  void dispose() {
+    super.dispose();
+    flickManager?.dispose();
+    videoPlayerController?.dispose();
   }
+
+  // init() async {
+  //   await _controller.startStreaming(
+  //     url: 'https://expense.lemonappsg.com/public/uploads/Test Video.mp4',
+  //     streamKey: '8BGYGRfPuqs6zaFHafdzNPgP6Uim44mkyKHHYkr1CfH',
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: ApiVideoCameraPreview(controller: _controller),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Container(
+          height: 300,
+          alignment: Alignment.center,
+          child: flickManager == null
+              ? CircularProgressIndicator(
+                  color: Colors.red,
+                )
+              : FlickVideoPlayer(
+                  flickManager: flickManager!,
+                  flickVideoWithControls: FlickVideoWithControls(),
+                ),
+        ),
       ),
     );
   }
